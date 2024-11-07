eb746503756073ee452f2520ec881c9f9ef881d3
cri-cov <cristian.covato@marconibari.edu.it>
2024-11-07 09:29:37 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int c, d, i, q, r;
+    cout << "inserisci un numero" << endl;
+    cin >> c;
+    i = 0;
+    for (d = 1; d <= c; d++) {
+        q = (int) ((double) c / d);
+        r = c % d;
+        if (r = 0) {
+            cout << d << " è divisore di" << c << endl;
+            i = i + 1;
+        }
+    }
+    if (i = 2) {
+        cout << c << "è un numero primo" << endl;
+    } else {
+        cout << c << "non è un numero primo" << endl;
+    }
+    return 0;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
